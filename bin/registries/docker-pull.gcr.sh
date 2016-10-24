86656350ec46dc00d341966ab2877c611e58a087
Philip Cristiano <philipcristiano@gmail.com>
2016-10-24 17:24:04 -0400

    Fix for a few shellcheck warnings
    squash shellshock

--- bin/registries/docker-pull.gcr.sh
+++ bin/registries/docker-pull.gcr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -z "$GCP_PROJECT" || -z "$GCP_CLUSTER"  || -z "$GCP_ZONE"]; then
+if [ -z "$GCP_PROJECT" || -z "$GCP_CLUSTER" || -z "$GCP_ZONE" ]; then
   echo "Missing GCP project, cluster, or zone!  Aborting"
   exit 1
 fi
@@ -14,9 +14,9 @@ if [ ! -d ~/google-cloud-sdk ]; then
   google-cloud-sdk/bin/gcloud components update #--version 119.0.0
 fi
 
-# TODO: Is this necessary if we have the kube config already set up for 
+# TODO: Is this necessary if we have the kube config already set up for
 # cluster connection?
 #~/google-cloud-sdk/bin/gcloud auth activate-service-account $ACCOUNT_EMAIL --key-file $KEYFILE
-~/google-cloud-sdk/bin/gcloud config set project $GCP_PROJECT
-~/google-cloud-sdk/bin/gcloud config set cluster $GCP_CLUSTER
-~/google-cloud-sdk/bin/gcloud config set zone $GCP_ZONE
+~/google-cloud-sdk/bin/gcloud config set project "${GCP_PROJECT}"
+~/google-cloud-sdk/bin/gcloud config set cluster "${GCP_CLUSTER}"
+~/google-cloud-sdk/bin/gcloud config set zone "${GCP_ZONE}"
