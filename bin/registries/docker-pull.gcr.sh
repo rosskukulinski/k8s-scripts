71c6b2ee435e882730236eb784f9a2a0e1fb524a
Robert <robert.warmack@gmail.com>
2016-10-27 15:38:07 -0500

    Adding ENV variables to README.

--- bin/registries/docker-pull.gcr.sh
+++ bin/registries/docker-pull.gcr.sh
@@ -14,9 +14,6 @@ if [ ! -d ~/google-cloud-sdk ]; then
   ~/google-cloud-sdk/bin/gcloud components update #--version 119.0.0
 fi
 
-# TODO: Is this necessary if we have the kube config already set up for
-# cluster connection?
-
 KEYFILE=${HOME}/gcloud-service-key.json
 echo $GCLOUD_SERVICE_KEY | base64 --decode > "${KEYFILE}"
 ~/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file "${KEYFILE}"
