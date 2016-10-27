91223a4ddf795449ca3d94ba6f8a5374407f65ab
Robert <robert.warmack@gmail.com>
2016-10-27 18:48:57 -0500

    Updating to reference gcloud from the current directory.

--- bin/registries/docker-pull.gcr.sh
+++ bin/registries/docker-pull.gcr.sh
@@ -9,18 +9,18 @@ if ! hash pyopenssl 2>/dev/null; then
   pip install pyopenssl
 fi
 
-if [ ! -d ~/google-cloud-sdk ]; then
+if [ ! -d ./google-cloud-sdk ]; then
   curl https://sdk.cloud.google.com | bash;
-  ~/google-cloud-sdk/bin/gcloud components update #--version 119.0.0
+  ./google-cloud-sdk/bin/gcloud components update #--version 119.0.0
 fi
 
 KEYFILE=${HOME}/gcloud-service-key.json
 echo $GCLOUD_SERVICE_KEY | base64 --decode > "${KEYFILE}"
-~/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file "${KEYFILE}"
+./google-cloud-sdk/bin/gcloud auth activate-service-account --key-file "${KEYFILE}"
 
-~/google-cloud-sdk/bin/gcloud config set project "${GCP_PROJECT}"
-~/google-cloud-sdk/bin/gcloud config set container/cluster "${GCP_CLUSTER}"
-~/google-cloud-sdk/bin/gcloud config set compute/zone "${GCP_ZONE}"
+./google-cloud-sdk/bin/gcloud config set project "${GCP_PROJECT}"
+./google-cloud-sdk/bin/gcloud config set container/cluster "${GCP_CLUSTER}"
+./google-cloud-sdk/bin/gcloud config set compute/zone "${GCP_ZONE}"
 
 # Authorize the docker client to work with GCR
 gcloud docker --authorize-only
